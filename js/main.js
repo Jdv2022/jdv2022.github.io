b1e21785f4a4ccfe427c32468103dc936ff00dae
John Dennis C. Vistal <113178260+Jdv2022@users.noreply.github.com>
2023-11-25 04:14:17 -0800

    Update main.js V.5

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(window).scroll(function(){
 const accessToken = 'hardCodedHehe';
 console.log('this is executed outside');
 if($('html').is(':visible')){ 
-    console.log('This is executed!);
+    console.log('This is executed!');
     $.ajax({    
         url: 'https://api-visit.jdv2022-projects.tech',
             method: 'POST',
